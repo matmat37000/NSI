a9fc4303da3eb31e64959e8bef9b0cc9c48d8c3f
Mathieu Bordier-Aupy <mathieu.bordieraupy@gmail.com>
2024-09-04 11:35:22 +0200

    Add ex 11

--- Exercices_S01-01/exercices.py
+++ Exercices_S01-01/exercices.py
@@ -13,11 +13,24 @@ def puissance(x: int, n: int) -> int:
     '''
     r: int = x
     for _ in range(1, n):
-        r = r * x
+        r *= x
 
     return r
 
 # EX 11
+def bissextile(a: int) -> bool:
+    '''
+    Take `a` as parameter, `a` need to be a year
+    return if `a` is a bissextile year (True/False)
+    >>> bissextile(2024)
+    True
+    >>> bissextile(300)
+    False
+    '''
+    if (a % 4 == 0 or a % 400 == 0) and (a % 100 != 0) :
+        return True
+    else:
+        return False
 
 
 
