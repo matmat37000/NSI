d6fbc8c86bd348494beeea13d28f60f8850c2e07
Mathieu Bordier-Aupy <mathieu.bordieraupy@gmail.com>
2024-09-04 19:24:04 +0200

    remove print

--- Exercices_S01-01/exercices.py
+++ Exercices_S01-01/exercices.py
@@ -5,6 +5,7 @@ from doctest import testmod
 
 # EX 10
 
+
 def puissance(x: int, n: int) -> int:
     '''
     Return the power of x by n
@@ -18,6 +19,8 @@ def puissance(x: int, n: int) -> int:
     return r
 
 # EX 11
+
+
 def bissextile(a: int) -> bool:
     '''
     Take `a` as parameter, `a` need to be a year
@@ -27,12 +30,14 @@ def bissextile(a: int) -> bool:
     >>> bissextile(300)
     False
     '''
-    if (a % 4 == 0 or a % 400 == 0) and (a % 100 != 0) :
+    if (a % 4 == 0 or a % 400 == 0) and (a % 100 != 0):
         return True
     else:
         return False
 
 # EX 12
+
+
 def nb_jour_annee(a: int) -> int:
     '''
     Return number of day in the year passed in `a`
@@ -47,10 +52,12 @@ def nb_jour_annee(a: int) -> int:
         return 365
 
 # EX 13
+
+
 def nb_jour_mois(a: int, m: int) -> int:
     '''
     Return the number of day in the month (m) with the year passed in a
-    
+
     >>> nb_jour_mois(2024, 2)
     29
     >>> nb_jour_mois(2023, 2)
@@ -62,14 +69,16 @@ def nb_jour_mois(a: int, m: int) -> int:
     '''
     if m == 2 and bissextile(a):
         return 29
-    elif m in [1, 3, 5, 7, 8, 10, 12]: # HACK: Hard coded value
+    elif m in [1, 3, 5, 7, 8, 10, 12]:  # HACK: Hard coded value
         return 31
-    elif m in [4, 6, 9, 11]: # HACK: Hard coded value
+    elif m in [4, 6, 9, 11]:  # HACK: Hard coded value
         return 30
     else:
         return 28
 
 # EX 14
+
+
 def nb_jours(j1: int, m1: int, a1: int, j2: int, m2: int, a2: int) -> int:
     '''
     Return days between j1/m1/a1 and j2/m2/a2
@@ -81,11 +90,9 @@ def nb_jours(j1: int, m1: int, a1: int, j2: int, m2: int, a2: int) -> int:
     for m in range(1, m2):
         annee2 += nb_jour_mois(a2, m)
 
-    print(annee1, annee2)
-
     annee3: int = 0
 
-    for year in range(a1+1 , a2):
+    for year in range(a1+1, a2):
         annee3 += nb_jour_annee(year)
 
     return annee1 + annee2 + annee3 + 1
