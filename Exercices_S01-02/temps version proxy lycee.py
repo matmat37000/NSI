4c470c79778ca8fb0c920ffc90026779f928c228
Mathieu Bordier-Aupy <mathieu.bordieraupy@gmail.com>
2024-09-05 17:52:07 +0200

    upload S01_02

---
+++ Exercices_S01-02/temps version proxy lycee.py
@@ -0,0 +1,40 @@
+import requests # le module requests permet de faire des requetes sur internet
+import json
+from datetime import datetime
+
+def temps(API, *args):
+    '''
+    affiche le temps de la ville
+    '''
+    print(args)
+    url = f'https://api.openweathermap.org/data/2.5/weather?lat={args[0]}&lon={args[1]}&appid={API}&units=metric'
+    proxy = {"http":"proxy:8080"} # on ajoute une variable pour configurer le proxy du lycée
+    r = requests.get(url) # cette fonction admet 2 arguments : url et proxies (optionnel)
+    return r.json()
+temps_tours=temps('3f786cef8456dea0c16e0e251d224a8d',47.4371991,0.68595)
+# print(temps_tours)
+print(f"Vous êtes à {temps_tours["name"]}, il fait {temps_tours['main']['temp']}°c. Le soleil se couche à {datetime.fromtimestamp(temps_tours['sys']['sunset'])}")
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+
+
+print()
+print(temps_tours["main"])
+print()
+print(temps_tours["main"]["temp"])
+
+
+  """    
+
---
+++ Exercices_S01-02/temps.py
@@ -0,0 +1,34 @@
+import requests # le module requests permet de faire des requetes sur internet
+def temps(clef, ville):
+    '''
+    affiche le temps de la ville
+    '''
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={ville}&units=metric&appid={clef}'
+    proxy = {"http":"proxy:8080"} # on ajoute une variable pour configurer le proxy du lycée
+    r = requests.get(url,proxies=proxy) # cette fonction admet 2 arguments : url et proxies (optionnel)
+    return r.json()
+temps_tours=temps('3f786cef8456dea0c16e0e251d224a8d','Tours')
+print(temps_tours)
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+print()
+print(temps_tours["main"])
+print()
+print(temps_tours["main"]["temp"])
+
+
+  """    
+
